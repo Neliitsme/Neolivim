84c43a9960a49c978994b7278792b62f10003980
Nelite <neliitsme14@gmail.com>
2022-07-15 19:33:21 +0300

    add .stylua.toml and force style

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -5,147 +5,143 @@ vim.cmd([[
   augroup end
 ]])
 
-local use = require("packer").use
-return require("packer").startup(function()
-	use("wbthomason/packer.nvim")
-	use({
-		"williamboman/nvim-lsp-installer",
-		{
-			"neovim/nvim-lspconfig",
-		},
-	})
-	use("ayu-theme/ayu-vim")
-	use("kyazdani42/nvim-web-devicons")
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
-	use({
-		"SmiteshP/nvim-gps",
-		disable = true,
-	})
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = "nvim-lua/plenary.nvim",
-	})
-	use("plasticboy/vim-markdown")
-	use({
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	})
-	use({
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("indent_blankline").setup({
-				show_current_context = true,
-				show_current_context_start = true,
-			})
-		end,
-	})
-	use("sbdchd/neoformat")
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && yarn install",
-		setup = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	})
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				on_attach = function(bufnr)
-					local gs = package.loaded.gitsigns
+local use = require('packer').use
+return require('packer').startup(function()
+    use('wbthomason/packer.nvim')
+    use({
+        'williamboman/nvim-lsp-installer',
+        {
+            'neovim/nvim-lspconfig',
+        },
+    })
+    use('ayu-theme/ayu-vim')
+    use('kyazdani42/nvim-web-devicons')
+    use({
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    })
+    use({
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+    })
+    use({
+        'nvim-telescope/telescope.nvim',
+        requires = 'nvim-lua/plenary.nvim',
+    })
+    use('plasticboy/vim-markdown')
+    use({
+        'windwp/nvim-autopairs',
+        config = function()
+            require('nvim-autopairs').setup({})
+        end,
+    })
+    use({
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            require('indent_blankline').setup({
+                show_current_context = true,
+                show_current_context_start = true,
+            })
+        end,
+    })
+    use('sbdchd/neoformat')
+    use({
+        'iamcco/markdown-preview.nvim',
+        run = 'cd app && yarn install',
+        setup = function()
+            vim.g.mkdp_filetypes = { 'markdown' }
+        end,
+        ft = { 'markdown' },
+    })
+    use({
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup({
+                on_attach = function(bufnr)
+                    local gs = package.loaded.gitsigns
 
-					local function map(mode, l, r, opts)
-						opts = opts or {}
-						opts.buffer = bufnr
-						vim.keymap.set(mode, l, r, opts)
-					end
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
 
-					-- Navigation
-					map("n", "]c", function()
-						if vim.wo.diff then
-							return "]c"
-						end
-						vim.schedule(function()
-							gs.next_hunk()
-						end)
-						return "<Ignore>"
-					end, { expr = true })
+                    -- Navigation
+                    map('n', ']c', function()
+                        if vim.wo.diff then
+                            return ']c'
+                        end
+                        vim.schedule(function()
+                            gs.next_hunk()
+                        end)
+                        return '<Ignore>'
+                    end, { expr = true })
 
-					map("n", "[c", function()
-						if vim.wo.diff then
-							return "[c"
-						end
-						vim.schedule(function()
-							gs.prev_hunk()
-						end)
-						return "<Ignore>"
-					end, { expr = true })
+                    map('n', '[c', function()
+                        if vim.wo.diff then
+                            return '[c'
+                        end
+                        vim.schedule(function()
+                            gs.prev_hunk()
+                        end)
+                        return '<Ignore>'
+                    end, { expr = true })
 
-					-- Actions
-					map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-					map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-					map("n", "<leader>hS", gs.stage_buffer)
-					map("n", "<leader>hu", gs.undo_stage_hunk)
-					map("n", "<leader>hR", gs.reset_buffer)
-					map("n", "<leader>hp", gs.preview_hunk)
-					map("n", "<leader>hb", function()
-						gs.blame_line({ full = true })
-					end)
-					map("n", "<leader>tb", gs.toggle_current_line_blame)
-					map("n", "<leader>hd", gs.diffthis)
-					map("n", "<leader>hD", function()
-						gs.diffthis("~")
-					end)
-					map("n", "<leader>td", gs.toggle_deleted)
+                    -- Actions
+                    map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+                    map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+                    map('n', '<leader>hS', gs.stage_buffer)
+                    map('n', '<leader>hu', gs.undo_stage_hunk)
+                    map('n', '<leader>hR', gs.reset_buffer)
+                    map('n', '<leader>hp', gs.preview_hunk)
+                    map('n', '<leader>hb', function()
+                        gs.blame_line({ full = true })
+                    end)
+                    map('n', '<leader>tb', gs.toggle_current_line_blame)
+                    map('n', '<leader>hd', gs.diffthis)
+                    map('n', '<leader>hD', function()
+                        gs.diffthis('~')
+                    end)
+                    map('n', '<leader>td', gs.toggle_deleted)
 
-					-- Text object
-					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-				end,
-			})
-		end,
-	})
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("onsails/lspkind.nvim")
-	use("rafamadriz/friendly-snippets")
-	use({
-		"L3MON4D3/LuaSnip",
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	})
-	use("saadparwaiz1/cmp_luasnip")
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	})
-	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup()
-		end,
-	})
-	use({
-		"folke/todo-comments.nvim",
-		requires = "nvim-lua/plenary.nvim",
-		config = function()
-			require("todo-comments").setup()
-		end,
-	})
+                    -- Text object
+                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                end,
+            })
+        end,
+    })
+    use('hrsh7th/cmp-nvim-lsp')
+    use('hrsh7th/cmp-buffer')
+    use('hrsh7th/cmp-path')
+    use('hrsh7th/cmp-cmdline')
+    use('hrsh7th/nvim-cmp')
+    use('onsails/lspkind.nvim')
+    use('rafamadriz/friendly-snippets')
+    use({
+        'L3MON4D3/LuaSnip',
+        config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+        end,
+    })
+    use('saadparwaiz1/cmp_luasnip')
+    use({
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end,
+    })
+    use({
+        'folke/trouble.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('trouble').setup()
+        end,
+    })
+    use({
+        'folke/todo-comments.nvim',
+        requires = 'nvim-lua/plenary.nvim',
+        config = function()
+            require('todo-comments').setup()
+        end,
+    })
 end)
