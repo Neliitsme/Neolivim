2ab86b11ab1e2b39c0f10c93d59e8192ffa1ce61
Nelite <neliitsme14@gmail.com>
2022-08-16 19:44:43 +0300

    feat: change style of bufferline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -167,10 +167,13 @@ local startup = require('packer').startup(function()
             require('bufferline').setup({
                 options = {
                     diagnostics = 'nvim_lsp',
-                    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                    diagnostics_indicator = function(count, level)
                         local icon = level:match('error') and ' ' or ' '
                         return ' ' .. icon .. count
                     end,
+                    separator_style = 'slant',
+                    show_buffer_close_icons = false,
+                    show_close_icon = false,
                 },
             })
         end,
