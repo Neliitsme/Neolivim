536357d113140e99197cc732672862d66a707b4d
Nelite <neliitsme14@gmail.com>
2022-08-17 20:10:37 +0300

    feat: change Trouble behaviour on keymap use

--- lua/core/keymappings.lua
+++ lua/core/keymappings.lua
@@ -21,7 +21,7 @@ vim.cmd([[
 ]])
 
 -- Trouble
-vim.api.nvim_set_keymap('n', '<leader>xx', '<cmd>Trouble<cr>', { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>xx', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>xw', '<cmd>Trouble workspace_diagnostics<cr>', { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>xd', '<cmd>Trouble document_diagnostics<cr>', { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>xl', '<cmd>Trouble loclist<cr>', { silent = true, noremap = true })
