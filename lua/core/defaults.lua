80e744b26b88e5cc9aa0eab4e0c055a9cb9317e7
Nelite <neliitsme14@gmail.com>
2022-08-17 20:08:01 +0300

    feat: remove off focus darkening

--- lua/core/defaults.lua
+++ lua/core/defaults.lua
@@ -14,13 +14,6 @@ vim.opt.smartindent = true
 -- Color scheme setup
 require('ayu').setup({
     mirage = true,
-    overrides = function()
-        if vim.o.background == 'dark' then
-            return { NormalNC = { bg = '#0f151e', fg = '#808080' } }
-        else
-            return { NormalNC = { bg = '#f0f0f0', fg = '#808080' } }
-        end
-    end,
 })
 require('ayu').colorscheme()
 
